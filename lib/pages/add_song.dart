6bc907cdcd883a4d346a81e791c356272bed3e43
Anderson Lemke <lemke@taller.net.br>
2022-03-07 21:39:38 -0300

    feat: song saved in library

--- lib/pages/add_song.dart
+++ lib/pages/add_song.dart
@@ -1,34 +1,42 @@
+import 'package:beathouse/providers/adding_song_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AddSong extends StatelessWidget {
   const AddSong({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add song'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // saveSong();
-          Navigator.pop(context);
-        },
-        child: const Icon(Icons.save),
-      ),
-      body: Form(
+    return ChangeNotifierProvider(
+      create: (_) => AddingSongProvider(),
+      builder: (context, child) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Add song'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.read<AddingSongProvider>().saveSong();
+            Navigator.pop(context);
+          },
+          child: const Icon(Icons.save),
+        ),
+        body: Form(
           child: Column(
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Name'),
+            children: [
+              TextFormField(
+                controller: context.read<AddingSongProvider>().nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              TextFormField(
+                controller: context.read<AddingSongProvider>().rateController,
+                decoration: const InputDecoration(labelText: 'Rate'),
+                keyboardType: const TextInputType.numberWithOptions(
+                    decimal: false, signed: false),
+              )
+            ],
           ),
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Beat'),
-            keyboardType: const TextInputType.numberWithOptions(
-                decimal: false, signed: false),
-          )
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
---
+++ lib/providers/adding_song_provider.dart
@@ -0,0 +1,22 @@
+import 'package:beathouse/model/folder.dart';
+import 'package:beathouse/model/song.dart';
+import 'package:beathouse/services/song_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:uuid/uuid.dart';
+
+class AddingSongProvider extends ChangeNotifier {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _rateController = TextEditingController();
+
+  void saveSong() async {
+    String name = _nameController.text.toString();
+    int rate = int.parse(_rateController.text.toString());
+    Song song = Song(const Uuid().v4(), name, rate, 'artist', Folder('folder'));
+    await SongService().save(song);
+    print('salvou');
+  }
+
+  get nameController => _nameController;
+
+  get rateController => _rateController;
+}
