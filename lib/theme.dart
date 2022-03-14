f8ad3a89be13f5d3b0dfa427c62d8d6b5cc8bed3
Anderson Lemke <lemke@taller.net.br>
2022-03-14 08:29:33 -0300

    refactor: change background color

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color primaryColor = const Color(0xFFFFFF00);
-Color backgroundColor = const Color(0x61000000);
+Color backgroundColor = const Color(0x15000000);
 
 ThemeData theme = ThemeData(
   primaryColor: primaryColor,
